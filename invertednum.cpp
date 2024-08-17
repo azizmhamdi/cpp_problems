85c5a8cd7c2385621563071dd7310638ac8ca766
aziz mhamdi <mhamdiaziz2004@gmail.com>
2024-08-17 10:16:37 +0100

    Added code for reading a positive number and printing an inverted character pattern

---
+++ invertednum.cpp
@@ -0,0 +1,29 @@
+ 
+#include <iostream>
+
+using namespace std;
+
+int ReadPositiveNumber(string Message) {
+    int Number = 0;
+    do {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
+    return Number;
+}
+
+void printInverted(int number) {
+    for (int i = 1; i <= number  ; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << char(65 + i - 1 ); // Convert i to corresponding uppercase letter
+        }
+        cout << endl;
+    }
+}
+ 
+
+int main() {
+    int number = ReadPositiveNumber("Enter a positive number:"); // Read the number and store it
+    printInverted(number); // Print the inverted pattern
+    return 0;
+}
